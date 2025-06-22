3f53f9b03b18a8e248e98dd9244cdfba9f3bba16
Rahuljain16 <rj267043@gmail.com>
2025-06-23 00:56:18 +0530

    latest

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("/listings", async (req,res) => {
 app.get("/listings/:id", async (req,res) =>{
     let{id} = req.params;
     const listing = await Listing.findById(id);
-    res.render("show.ejs",{listing});
+    res.render("listing/show.ejs",{ listing });
 })
 /*app.get("/testListing", async (req,res) => {
     let sampleListing = new Listing({
