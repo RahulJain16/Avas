52ad655a0437b7abc7eab30a6c8a09096cf21282
Rahuljain16 <rj267043@gmail.com>
2025-06-22 15:31:59 +0530

    latest

--- models/listing.js
+++ models/listing.js
@@ -2,13 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true,
+    },
     description: String,
-    image: String,
+    image: {
+        type: String,
+        set: (v) => v ==="" ? "https://unsplash.com/photos/a-view-of-the-sky-from-above-the-clouds-8xXeYkZMm-c" : v,
+    },
     price: Number,
     location: String,
     country: String,
 });
 
 const Listing = mongoose.model("Listing",listingSchema);
-modules.export = Listing;
+module.export = Listing;
