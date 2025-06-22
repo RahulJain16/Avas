fd100d9baa4ac359185446c9d7ac2da1b2f29111
Rahuljain16 <rj267043@gmail.com>
2025-06-23 00:51:03 +0530

    latest

--- app.js
+++ app.js
@@ -22,16 +22,24 @@ async function main() {
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
 
 app.get("/",(req,res) => {
     res.send("hi i am root");
 });
 
+//Index Route
 app.get("/listings", async (req,res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index.ejs",allListings);
+    res.render("listings/index.ejs",{allListings});
 })
 
+//SHOW ROUTE
+app.get("/listings/:id", async (req,res) =>{
+    let{id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("show.ejs",{listing});
+})
 /*app.get("/testListing", async (req,res) => {
     let sampleListing = new Listing({
         title: "My New Villa",
