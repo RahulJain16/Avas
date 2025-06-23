a9735b7deb78c6a498933a2d0fde9623c7152d07
Rahuljain16 <rj267043@gmail.com>
2025-06-24 02:11:33 +0530

    latest

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.engine('ejs', ejsMate);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
 
 
 app.get("/",(req,res) => {
