ec5c45074d89d07eb25e5b11800c8bc46b147fe7
Rahuljain16 <rj267043@gmail.com>
2025-06-24 00:57:08 +0530

    latest

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const initdata = require("./init/data.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
