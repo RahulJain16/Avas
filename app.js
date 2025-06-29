4e43ff952b05138122b1fa56f1ed56bc2d5c766c
Rahuljain16 <rj267043@gmail.com>
2025-06-30 00:42:30 +0530

    latest

--- app.js
+++ app.js
@@ -33,16 +33,23 @@ app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"/public")));
 
+app.use((req, res, next) => {
+    console.log("[REQUEST URL]", req.method, req.url);
+    next();
+});
+
+
 
 app.get("/",(req,res) => {
     res.send("hi i am root");
 });
 
 //Index Route
-app.get("/listings", async (req,res) => {
+app.get("/listings", wrapAsync(async (req,res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-});
+})
+);
 
 //New Route
 app.get("/listings/new", (req,res) =>{
@@ -51,11 +58,12 @@ app.get("/listings/new", (req,res) =>{
 
 
 //SHOW ROUTE
-app.get("/listings/:id", async (req,res) =>{
+app.get("/listings/:id",wrapAsync(async (req,res) =>{
     let{id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{ listing });
-});
+})
+);
 
 //Create Route
 app.post("/listings", wrapAsync(async (req,res) => {
@@ -66,26 +74,28 @@ app.post("/listings", wrapAsync(async (req,res) => {
 );
 
 //Edit Route
-app.get("/listings/:id/edit", async (req,res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+})
+);
 
 //Update Route
-app.put("/listings/:id", async (req,res) => {
+app.put("/listings/:id", wrapAsync(async (req,res) => {
     const { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect("/listings");
-});
+})
+);
 
 //Delete Route
-app.delete("/listings/:id", async (req,res) =>{
+app.delete("/listings/:id", wrapAsync(async (req,res) =>{
     const { id } = req.params;
     let listing=await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-
 })
+);
 
 /*app.get("/testListing", async (req,res) => {
     let sampleListing = new Listing({
@@ -101,15 +111,15 @@ app.delete("/listings/:id", async (req,res) =>{
     res.send("successful testing");
 });
 */
-app.all("*",(req,res,next) =>{
+app.use((req,res,next) => {
     next(new ExpressError(404,"Page not found"));
-
-})
+});
 
 app.use((err,req,res,next) => {
     let{statusCode,message}=err;
     res.status(statusCode).send(message);
 })
+
 app.listen(8080,() =>{
     console.log("server is listening");
 });
