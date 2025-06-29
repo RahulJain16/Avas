b528a67fc068b385d311e396ad4fae0abf933b2f
Rahuljain16 <rj267043@gmail.com>
2025-06-30 00:54:59 +0530

    latest

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.use((req,res,next) => {
 });
 
 app.use((err,req,res,next) => {
-    let{statusCode,message}=err;
+    let{statusCode=500,message="something went wrong"}=err;
     res.status(statusCode).send(message);
 })
 
