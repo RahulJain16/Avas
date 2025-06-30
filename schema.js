460b4b01339a522fb70c130cdd6e426b53d59699
Rahuljain16 <rj267043@gmail.com>
2025-06-30 19:14:12 +0530

    latest

--- schema.js
+++ schema.js
@@ -1,1 +1,9 @@
-const joi = 
+const joi = require('joi');
+
+const listingSchema = Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location:
+    }).required()
+})
