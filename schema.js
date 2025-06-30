c67cbeb13767cb23bcfc63ff520ceccfddf31512
Rahuljain16 <rj267043@gmail.com>
2025-06-30 19:17:31 +0530

    latest

--- schema.js
+++ schema.js
@@ -4,6 +4,9 @@ const listingSchema = Joi.object({
     listing : Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
-        location:
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required(),
+        image: Joi.string().allow("",null)
     }).required()
 })
