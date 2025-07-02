3e6d1b92a2fa1dbb62a69afc85d9b141c2b20ed4
Rahuljain16 <rj267043@gmail.com>
2025-07-02 23:00:48 +0530

    latest

--- models/review.js
+++ models/review.js
@@ -5,6 +5,13 @@ const reviewSchema = new Schema({
     comment: String,
     rating:{
         type: Number,
-        min:
+        min:1,
+        max:5
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
     }
-})
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
