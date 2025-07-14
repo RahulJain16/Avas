5c7542224e81da90e19ce95a569ff8e9e798c31b
Rahuljain16 <rj267043@gmail.com>
2025-07-15 00:24:08 +0530

    latest

--- init/index.js
+++ init/index.js
@@ -18,8 +18,11 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initdata.data = initdata.data.map((obj) => ({...obj, owner: new mongoose.Types.ObjectId("68583ae8dd8c787a1803faa5")}));
     await Listing.insertMany(initdata.data);
     console.log("data was  initialized");
 };
 
-initDB();
+initDB().then(() => {
+    mongoose.connection.close();
+});
